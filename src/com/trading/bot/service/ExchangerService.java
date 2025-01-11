a6dc478f4ae59a7c9e5a24760b3487dc6be17bb2
Ilyas Makirov <ilmak1704@gmail.com>
2025-01-11 18:15:48 +0500

    added Exchanger Service

---
+++ src/com/trading/bot/service/ExchangerService.java
@@ -0,0 +1,8 @@
+package com.trading.bot.service;
+
+import com.trading.bot.model.Order;
+
+public interface ExchangerService {
+    void placeOrder(Order order);
+    void executeOrder(Order order);
+}

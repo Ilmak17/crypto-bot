b6842f8a226b699cb2965b7d2b749c8852e5e2b8
Ilyas Makirov <ilmak1704@gmail.com>
2025-02-09 21:04:26 +0500

    kafka enhancement

--- src/main/java/com/trading/bot/event/KafkaEventPublisher.java
+++ src/main/java/com/trading/bot/event/KafkaEventPublisher.java
@@ -2,29 +2,30 @@ package com.trading.bot.event;
 
 import com.trading.bot.model.enums.OrderEvent;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
 
 public class KafkaEventPublisher {
-//    private final KafkaProducer<String, String> producer;
-//    private final String topic;
+    private final KafkaProducer<String, String> producer;
+    private final String topic;
 
     public KafkaEventPublisher(String topic) {
-//        this.topic = topic;
-//
-//        Properties props = new Properties();
-//        props.put("bootstrap.servers", "localhost:9092");
-//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        producer = new KafkaProducer<>(props);
+        this.topic = topic;
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        producer = new KafkaProducer<>(props);
     }
 
     public void publish(OrderEvent key, String message) {
-//        producer.send(new ProducerRecord<>(topic, key, message));
+        producer.send(new ProducerRecord<>(topic, key, message));
     }
 
     public void close() {
-//        producer.close();
+        producer.close();
     }
 }

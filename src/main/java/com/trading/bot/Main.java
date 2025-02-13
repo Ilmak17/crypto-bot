2fe294f1ca7b593a0b89032fa10b9b7e2c451984
Ilyas Makirov <ilmak1704@gmail.com>
2025-02-13 19:42:39 +0500

    refactoring

--- src/main/java/com/trading/bot/Main.java
+++ src/main/java/com/trading/bot/Main.java
@@ -1,13 +1,6 @@
 package com.trading.bot;
 
-
-import com.trading.bot.model.enums.Symbol;
-import com.trading.bot.service.ExchangerService;
-import com.trading.bot.service.ExchangerServiceBean;
-
 public class Main {
     public static void main(String[] args) {
-        ExchangerService exchangerService = new ExchangerServiceBean();
-        exchangerService.initializeOrderBook(Symbol.BTCUSDT.toString(), 100);
     }
 }
--- src/main/java/com/trading/bot/service/ExchangerService.java
+++ src/main/java/com/trading/bot/service/ExchangerService.java
@@ -6,15 +6,10 @@ import com.trading.bot.model.Order;
 
 public interface ExchangerService {
     void registerBot(Bot bot);
-
     void startBots();
-
     void placeOrder(Order order);
-
     void executeOrders();
-
     void getOrderBook();
-
     boolean cancelOrder(Long orderId);
 
     void stopAutoUpdate();

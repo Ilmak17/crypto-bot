9ffccd9d693d80840d606dd55321e81eec6a5233
Ilyas Makirov <ilmak1704@gmail.com>
2025-03-03 19:42:23 +0500

    refactoring dump bot

--- src/main/java/com/trading/bot/bots/DumbBotBean.java
+++ src/main/java/com/trading/bot/bots/DumbBotBean.java
@@ -10,7 +10,6 @@ import com.trading.bot.service.ExchangerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 

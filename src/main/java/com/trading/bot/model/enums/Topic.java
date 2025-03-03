b0224add903cc5a727256f14aaf64f4179197dc6
Ilyas Makirov <ilmak1704@gmail.com>
2025-03-03 19:34:58 +0500

    added new topics

--- src/main/java/com/trading/bot/model/enums/Topic.java
+++ src/main/java/com/trading/bot/model/enums/Topic.java
@@ -1,8 +1,17 @@
 package com.trading.bot.model.enums;
 
 public enum Topic {
-    ORDER_CANCELLED,
-    ORDER_FILLED,
-    ORDER_PLACED,
+    PRICE_UPDATES("price-updates"),
+    ORDER_EVENTS("order-events"),
+    TRADE_EXECUTIONS("trade-executions");
 
+    private final String topicName;
+
+    Topic(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
 }

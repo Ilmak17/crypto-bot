af2c7d8b89da7fc2d553c7b0889560b1ceef5851
Ilyas Makirov <ilmak1704@gmail.com>
2025-03-11 22:14:20 +0500

    updated trading simulator

--- src/main/java/com/trading/bot/service/TradingSimulatorServiceBean.java
+++ src/main/java/com/trading/bot/service/TradingSimulatorServiceBean.java
@@ -2,6 +2,7 @@ package com.trading.bot.service;
 
 import com.trading.bot.api.BinanceApiClient;
 import com.trading.bot.bots.Bot;
+import com.trading.bot.event.KafkaEventPublisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,11 +11,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import static com.trading.bot.model.enums.Topic.PRICE_UPDATES;
+
 public class TradingSimulatorServiceBean implements TradingSimulatorService {
     private final BinanceApiClient binanceApiClient;
     private final ExchangerServiceBean exchangerService;
     private final ScheduledExecutorService scheduler;
     private final List<Bot> bots;
+    private final KafkaEventPublisher kafkaEventPublisher;
     private static final Logger logger = LoggerFactory.getLogger(TradingSimulatorServiceBean.class);
 
     public TradingSimulatorServiceBean(BinanceApiClient binanceApiClient, ExchangerServiceBean exchangerService, List<Bot> bots) {
@@ -22,6 +26,7 @@ public class TradingSimulatorServiceBean implements TradingSimulatorService {
         this.exchangerService = exchangerService;
         this.bots = bots;
         this.scheduler = Executors.newScheduledThreadPool(1);
+        this.kafkaEventPublisher = new KafkaEventPublisher();
     }
 
     @Override
@@ -34,8 +39,9 @@ public class TradingSimulatorServiceBean implements TradingSimulatorService {
             try {
                 double price = binanceApiClient.getPrice();
                 logger.info("Current BTC price: {}", price);
-
+                kafkaEventPublisher.publish(PRICE_UPDATES, "BTCUSDT: " + price);
                 bots.forEach(bot -> bot.performAction(price));
+
             } catch (Exception e) {
                 logger.error("Error fetching price from Binance", e);
             }

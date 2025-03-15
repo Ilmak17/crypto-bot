2e5ab2003696d3ea0defb1d120043367566d0dd6
Ilyas Makirov <ilmak1704@gmail.com>
2025-03-15 23:55:40 +0500

    added binance api library

--- src/main/java/com/trading/bot/Main.java
+++ src/main/java/com/trading/bot/Main.java
@@ -1,6 +1,11 @@
 package com.trading.bot;
 
+import com.trading.bot.api.BinanceApiClient;
+import com.trading.bot.api.BinanceApiClientBean;
+
 public class Main {
     public static void main(String[] args) {
+        BinanceApiClientBean binanceApiClient = new BinanceApiClientBean();
+        binanceApiClient.getTestBalance();
     }
 }
--- src/main/java/com/trading/bot/api/BinanceApiClientBean.java
+++ src/main/java/com/trading/bot/api/BinanceApiClientBean.java
@@ -1,26 +1,54 @@
 package com.trading.bot.api;
 
+import com.binance.connector.client.SpotClient;
+import com.binance.connector.client.impl.SpotClientImpl;
 import com.trading.bot.api.dto.CancelOrderDto;
 import com.trading.bot.api.dto.OrderBookDto;
 import com.trading.bot.api.dto.PlaceOrderDto;
-import com.trading.bot.api.mapper.OrderBookDtoMapper;
-import com.trading.bot.api.mapper.PriceDtoMapper;
-import com.trading.bot.api.util.BinanceSignature;
 import com.trading.bot.model.enums.Symbol;
+import io.github.cdimascio.dotenv.Dotenv;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.util.LinkedHashMap;
+
 
 public class BinanceApiClientBean implements BinanceApiClient {
-    private static final String API_KEY = "";
-    private static final String API_SECRET = "";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String BASE_URL = dotenv.get("BINANCE_BASE_URL");
+
+    private static final String API_KEY = dotenv.get("BINANCE_API_KEY");
+    private static final String SECRET_KEY = dotenv.get("BINANCE_SECRET_KEY");
 
     private final SpotClient client;
 
     public BinanceApiClientBean() {
-        this.client = new SpotClientImpl(API_KEY, SECRET_KEY);
+        this.client = new SpotClientImpl(API_KEY, SECRET_KEY, BASE_URL);
+    }
+    public void getTestBalance() {
+        LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+        params.put("symbol", Symbol.BTCUSDT.toString());
+        params.put("limit", 10);
+
+        String response = client.createMarket().depth(params);
+        System.out.println(response);
+    }
+
+    @Override
+    public Double getPrice() {
+        return 0.0;
+    }
+
+    @Override
+    public OrderBookDto getOrderBook(Symbol market, int limit) {
+        return null;
+    }
+
+    @Override
+    public void placeOrder(PlaceOrderDto dto) {
+
+    }
+
+    @Override
+    public void cancelOrder(CancelOrderDto dto) {
+
     }
 }

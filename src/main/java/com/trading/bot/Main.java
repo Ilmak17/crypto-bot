6495b9584851b2fd8c121ef2c857b016bffed98c
Ilyas Makirov <ilmak1704@gmail.com>
2025-03-30 18:50:08 +0500

    refactoring

--- src/main/java/com/trading/bot/Main.java
+++ src/main/java/com/trading/bot/Main.java
@@ -1,10 +1,7 @@
 package com.trading.bot;
 
-import com.trading.bot.api.BinanceApiClient;
 import com.trading.bot.api.BinanceApiClientBean;
-import com.trading.bot.api.dto.CancelOrderDto;
 import com.trading.bot.api.dto.OrderBookDto;
-import com.trading.bot.api.dto.PlaceOrderDto;
 import com.trading.bot.model.enums.Symbol;
 
 public class Main {
@@ -18,18 +15,5 @@ public class Main {
         System.out.println("\nFetching Order Book...");
         OrderBookDto orderBook = binanceClient.getOrderBook(Symbol.BTCUSDT, 5);
         System.out.println("Order Book: " + orderBook);
-
-        System.out.println("\nPlacing test order...");
-        PlaceOrderDto orderDto = new PlaceOrderDto(
-                Symbol.BTCUSDT.toString(),
-                "BUY",
-                "limit",
-                0.001,
-                price * 0.99
-        );
-        binanceClient.placeOrder(orderDto);
-
-        System.out.println("\nCancelling order...");
-         binanceClient.cancelOrder(new CancelOrderDto(Symbol.BTCUSDT.toString(), "order_id"));
     }
 }

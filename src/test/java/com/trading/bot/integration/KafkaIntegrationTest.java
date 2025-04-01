f206de6df343b7049660a6f853246754df874ce1
Ilyas Makirov <ilmak1704@gmail.com>
2025-04-01 23:39:21 +0500

    created kafka integration test

---
+++ src/test/java/com/trading/bot/integration/KafkaIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.trading.bot.integration;
+
+import org.junit.jupiter.api.TestInstance;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class KafkaIntegrationTest {
+
+
+}

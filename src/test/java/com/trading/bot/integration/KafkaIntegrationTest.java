6540c54a500402ab1ef8a845c28a17b99a912c57
Ilyas Makirov <ilmak1704@gmail.com>
2025-04-01 23:40:56 +0500

    created kafka integration test

--- src/test/java/com/trading/bot/integration/KafkaIntegrationTest.java
+++ src/test/java/com/trading/bot/integration/KafkaIntegrationTest.java
@@ -1,11 +1,30 @@
 package com.trading.bot.integration;
 
+import com.trading.bot.event.KafkaEventPublisher;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class KafkaIntegrationTest {
+    @Container
+    public KafkaContainer kafka = new KafkaContainer("confluentinc/cp-kafka:7.5.0");
 
+    private KafkaEventPublisher publisher;
+
+    @BeforeAll
+    void setup() {
+        System.setProperty("KAFKA_BOOTSTRAP_SERVERS", kafka.getBootstrapServers());
+        publisher = new KafkaEventPublisher(kafka.getBootstrapServers());
+    }
+
+    @AfterAll
+    void tearDown() {
+        publisher.close();
+    }
 
 }

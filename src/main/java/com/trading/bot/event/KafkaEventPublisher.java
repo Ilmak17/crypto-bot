761da4bcf6bda100ce8cdaee6599fd280eed6e9b
Ilyas Makirov <ilmak1704@gmail.com>
2025-04-03 21:43:34 +0500

    some refactoring

--- src/main/java/com/trading/bot/event/KafkaEventPublisher.java
+++ src/main/java/com/trading/bot/event/KafkaEventPublisher.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Properties;
 
@@ -12,12 +13,21 @@ public class KafkaEventPublisher {
     private final KafkaProducer<String, String> producer;
 
     public KafkaEventPublisher(String bootstrapServers) {
+        producer = getStringStringKafkaProducer(bootstrapServers);
+    }
+
+    public KafkaEventPublisher() {
+        producer = getStringStringKafkaProducer("localhost:9092");
+    }
+
+    @NotNull
+    private KafkaProducer<String, String> getStringStringKafkaProducer(String bootstrapServers) {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        producer = new KafkaProducer<>(props);
+        return new KafkaProducer<>(props);
     }
 
     public void publish(Topic topic, String message) {

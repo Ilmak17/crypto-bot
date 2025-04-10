f2e7a85ed7ecb997b392dd4e196363f933dfd016
Ilyas Makirov <ilmak1704@gmail.com>
2025-04-10 20:16:28 +0500

    refactoring

--- src/main/java/com/trading/bot/event/KafkaEventPublisher.java
+++ src/main/java/com/trading/bot/event/KafkaEventPublisher.java
@@ -31,13 +31,14 @@ public class KafkaEventPublisher {
     private KafkaProducer<String, String> getStringStringKafkaProducer(String bootstrapServers) {
         Properties props = new Properties();
 
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
-        props.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
-
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        props.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
+        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5);
 
         return new KafkaProducer<>(props);
     }
